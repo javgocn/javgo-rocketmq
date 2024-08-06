50098e6ce82e454f12db2293200316eb27edb42a
javgo <javgocn@gmail.com>
2024-08-06 19:23:32 +0800

    feat: init

---
+++ src/main/java/cn/javgo/javgo/mq/rocketmq/JavgoRocketmqApplication.java
@@ -0,0 +1,13 @@
+package cn.javgo.javgo.mq.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavgoRocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavgoRocketmqApplication.class, args);
+    }
+
+}
